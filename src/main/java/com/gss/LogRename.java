0e254feb608ffba33a40d7074b79c6701c4ace7a
Nicole-GitHub <sandy770403@gmail.com>
2022-01-30 14:32:02 +0800

    update JobF.txt

--- src/main/java/com/gss/LogRename.java
+++ src/main/java/com/gss/LogRename.java
@@ -16,10 +16,10 @@ public class LogRename {
 	public static void logRename(String path, List<Map<String, String>> qcLogList, String downloadsPath) {
 		File f;
 		FileOutputStream fos = null;
-		Cell cell;
 		Workbook workbook = null;
 		Sheet sheet;
-		String run_flag = "", rqId = "", excelPath = "", filePath = "", txt = "" ;
+		String run_flag = "", rq_id = "", excelPath = "", filePath = "", txt = "",
+				control_id = "", source_tablenm = "", target_tablenm = "", exec_sdate = "", exec_edate = "";
 
 		try {
 			
@@ -33,22 +33,30 @@ public class LogRename {
 
 				for (Row row : sheet) {
 					if (row.getRowNum() > 1 && row.getCell(0) != null) {
-						rqId = row.getCell(2).getStringCellValue();
-						cell = row.getCell(10);
-						
-						run_flag = cell.getCellType() == Cell.CELL_TYPE_NUMERIC
+						control_id = row.getCell(1).getCellType() == Cell.CELL_TYPE_NUMERIC
+								? String.valueOf(row.getCell(1).getNumericCellValue())
+								: row.getCell(1).getStringCellValue();
+						rq_id = row.getCell(2).getStringCellValue();
+						source_tablenm = row.getCell(7).getStringCellValue();
+						target_tablenm = row.getCell(8).getStringCellValue();
+						run_flag = row.getCell(10).getCellType() == Cell.CELL_TYPE_NUMERIC
 								? String.valueOf(row.getCell(10).getNumericCellValue())
 								: row.getCell(10).getStringCellValue();
+						exec_sdate = row.getCell(18).getStringCellValue();
+						exec_edate = row.getCell(19).getStringCellValue();
 
 						// 將失敗的RQ Log加上run_flag結果
-						f = new File(filePath + rqId + ".log");
+						f = new File(filePath + rq_id + ".log");
 						if (f.exists())
-							f.renameTo(new File(filePath + run_flag + "_" + rqId + ".log"));
-						
+							f.renameTo(new File(filePath + run_flag + "_" + rq_id + ".log"));
+
 						// 將失敗的RQ另外整理寫入jobF.txt
-						if(run_flag.contains("3"))
-							txt += map.get("qcLogFile") + "\t" + run_flag + "\t" + rqId + " \r\n";
-						
+						if (run_flag.contains("3"))
+							txt += "JOB:\t" + map.get("qcLogFile") + " \r\n"
+									+ "RQ_ID:\t" + control_id + "\t" + rq_id + "\t" + run_flag + " \r\n"
+									+ "Table:\t" + source_tablenm + " -> " + target_tablenm + " \r\n"
+									+ "DateTime:\t" + exec_sdate + " -> " + exec_edate + " \r\n\r\n";
+
 					}
 				}
 				workbook.close();
